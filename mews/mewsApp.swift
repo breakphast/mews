523fece1150af93137d342fcaeaced94fc9fb9d4
Desmond Fitch <desmond.fitch@icloud.com>
2024-10-04 11:27:42 -0400

    Initial Commit

---
+++ mews/mewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mewsApp.swift
+//  mews
+//
+//  Created by Desmond Fitch on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct mewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
